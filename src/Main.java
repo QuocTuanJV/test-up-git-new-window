9a1924cced5d41c14b8566a56b767865f011fb10
QuocTuanJV <quoctuan.naf@gmail.com>
2019-11-15 10:35:30 +0700

    new up window test

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<Integer,String> myMap = new HashMap<>();
+        myMap.put(1,"A");
+        myMap.put(2,"B");
+        List<String> myList = new ArrayList<>(myMap.values());
+        System.out.println(myMap.values());
+        System.out.println(myList.get(0));
+        System.out.println(myList.get(1));
+    }
+}
